bb996c4dbbb357b8be79870c93d82f23774c8f0d
Thomastw374 <thomas.whiteley46@gmail.com>
2022-07-11 14:42:05 +0100

    basic translation implemented

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-const englishBox = document.querySelector("")
-
+const englishTextBox = document.querySelector(
+  ".translator-container__english-text-box"
+);
+const morseCodeTextBox = document.querySelector(
+  ".translator-container__morse-code-text-box"
+);
+const translateButton = document.querySelector(
+  ".translator-container__translate-button"
+);
 
 const morseCodeAlphabet = {
-    a: ".-",
-    b: "-...",
-    c: "-.-.",
-    d: "-..",
-    e: ".",
-    f: "..-.",
-    g: "--.",
-    h: "....",
-    i: "..",
-    j: ".---",
-    k: "-.-",
-    l: ".-..",
-    m: "--",
-    n: "-.",
-    o: "---",
-    p: ".--.",
-    q: "--.-",
-    r: ".-.",
-    s: "...",
-    t: "-",
-    u: "..-",
-    v: "...-",
-    x: "-..-",
-    y: "-.--",
-    z: "--..",
-    fullstop: ".-.-.-",
-    comma: "--..--",
-    question: "..--.."
-
-}
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  fullstop: ".-.-.-",
+  comma: "--..--",
+  question: "..--..",
+};
 
 export const translateMorseCode = (event) => {
+  englishString = englishTextBox.value;
+  console.log(englishString)
+  let morseCodeArr = []
 
-}
+  for (i=0; i<englishString.length; i++) {
+    morseCodeArr.push(morseCodeAlphabet[englishString[i]]);
+  }
+
+  morseCodeTextBox.value = morseCodeArr.join(" ")
+};
+
+translateButton.addEventListener("click", translateMorseCode);
--- script.spec.js
+++ script.spec.js
@@ -1,4 +1,12 @@
 import { expect, it } from "@jest/globals";
 import { translateMorseCode } from "./script";
 
-it("should translate 'Hello' to '.... . .-.. .-.. ---")
+it("should translate 'Hello' to '.... . .-.. .-.. ---", () => {
+    const result = translateMorseCode("Hello");
+    expect(result).toBe(".... . .-.. .-.. ---");
+})
+
+// it("should alert 'text empty' when english input is empty", () => {
+//     const result = translateMorseCode("")
+//     expect(result).toBe("")
+// })
